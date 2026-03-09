81f67fab9675bb387bf1bb6f6ac9c67465060b25
yunsy1103 <127287492+yunsy1103@users.noreply.github.com>
2026-03-09 23:42:09 +0900

    [Silver V] Title: 숫자 카드, Time: 884 ms, Memory: 136612 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.java
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.java
@@ -1,29 +1,36 @@
+// HashSet을 사용하면 리스트/배열보다 더 빠르게 탐색할 수 있어서 사용
+// HashSet -> contains()를 통해 한번에 탐색 가능 => 존재 여부 확인할 때 HashSet 떠올리기
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.HashSet;
-import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
 
-        int N =Integer.parseInt(br.readLine());
-        Set<Integer> cards = new HashSet<>();
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(br.readLine());
+        HashSet<Integer> set = new HashSet<>();
+
+        st = new StringTokenizer(br.readLine());
         for(int i=0;i<N;i++){
-            cards.add(Integer.parseInt(st.nextToken()));
+            set.add(Integer.parseInt(st.nextToken()));
         }
+        int M = Integer.parseInt(br.readLine());
 
-
-        int M =Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
         st = new StringTokenizer(br.readLine());
         for(int i=0;i<M;i++){
-            int target = Integer.parseInt(st.nextToken());
-            System.out.print((cards.contains(target) ? "1" : "0") + " ");
+            int num = Integer.parseInt(st.nextToken());
+            if(set.contains(num)){
+                sb.append(1).append(" ");
+            }
+            else {
+                sb.append(0).append(" ");
+            }
         }
 
-        
+        System.out.print(sb.toString());
     }
 }
